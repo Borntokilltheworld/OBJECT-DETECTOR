577371fccbd4627441e1cbc4a5cd3d94f22d9028
Borntokilltheworld <77736929+Borntokilltheworld@users.noreply.github.com>
2022-02-14 13:43:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,6 +11,18 @@ function preload(){
 }
 function draw(){
     image(img,0,0,640,420);
+
+    if(status != ""){
+        for(i = 0;i < objects.length;i++){
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%",objects[i].x + 15,objects[i].y+ 15);
+            noFill();
+            stroke("#FF0000")
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
     fill("#FF0000");
     text("dog",45,75);
     noFill();
