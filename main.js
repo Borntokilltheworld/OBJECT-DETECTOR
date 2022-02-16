29692b516b4eb07f64f604c8e72acc1647180db3
Borntokilltheworld <77736929+Borntokilltheworld@users.noreply.github.com>
2022-02-16 13:28:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,48 +2,44 @@ img ="";
 status = "";
 objects = [];
 function setup() {
-canvas = createCanvas(640,420);
+canvas = createCanvas(380,380);
 canvas.center();
-ObjectDetector = ml5.objectDetector('cocossd',modelLoaded);
-document.getElementById("status").innerHTML = "Object has been Detetced";
+video = createCapture(VIDEO);
+video.hide();
+objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+document.getElementById("status").innerHTML = "Object is being detected by COCO SSD";
 }
 function preload(){
    img = loadImage('dog_cat.jpg');
 }
 function draw(){
-    image(img,0,0,640,420);
+    image(video,0,0,380,380);
 
     if(status != ""){
-        for(i = 0;i < objects.length;i++){
+        objectDetector.detect(video,gotResult);
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        for( var i = 0;i < objects.length;i++){
             document.getElementById("status").innerHTML = "Status : Object Detected";
-            fill("#FF0000");
+            document.getElementById("number_of_objects").innerHTML = "Number of Objects detected by Coco SSD is " + objects.length;
+            fill(r,g,b);
             percent = floor(objects[i].confidence * 100);
-            text(objects[i].label + "" + percent + "%",objects[i].x + 15,objects[i].y+ 15);
+            text(objects[i].label + " " + percent + "%",objects[i].x ,objects[i].y+ 15);
             noFill();
-            stroke("#FF0000")
+            stroke(r,g,b);
             rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
         }
     }
-    fill("#FF0000");
-    text("dog",45,75);
-    noFill();
-    stroke("#FF0000");
-    rect(30,60,450,350);
-
-    fill("#FF0000");
-    text("Cat",320,120);
-    noFill();
-    stroke("#FF0000");
-    rect(300,90,270,320);
 }
 function modelLoaded(){
     console.log("Model Loaded!");
-    status = "true" ; 
-    objectDetector.detect(img,gotResult)
+    status = true ; 
+    objectDetector.detect(video,gotResult)
 }
 function gotResult(error,results){
 if(error){
-    console.log(error);
+    console.error(error);
 }
 else{
     console.log(results);
